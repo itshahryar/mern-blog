21f44e6161c4f867b54dfc7bcd5879be2c106917
itshahryar <shahryaramjadmos@gmail.com>
2024-08-20 04:46:47 +0500

    backend complete + footer UI/UX

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from './pages/Dashboard';
 // import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 // import PrivateRoute from './components/PrivateRoute';
 // import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 // import CreatePost from './pages/CreatePost';
@@ -30,7 +30,7 @@ export default function App() {
 
 
       </Routes>
-   
+        <Footer/>
     </BrowserRouter>
   );
 }
